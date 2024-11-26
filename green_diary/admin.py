73eb712fa139d082aaa95c63251bffaadebd42d9
Josher89 <j.hernandezruiz89@gmail.com>
2024-11-26 13:55:56 -0800

    Changed admin name to economiica and fixed the home page

--- green_diary/admin.py
+++ green_diary/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from .models import Entry
 
 admin.site.register(Entry)
+admin.site.site_header = "economiica Admin"
+admin.site.site_title = "economiica Admin"
+admin.site.index_title = "Welcom to the economiica Admin"
